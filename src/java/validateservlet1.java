84e75898f2404142394ba4f385599bdba4be3c0a
Administrator <Administrator@172.31.10.45>
2021-02-15 18:30:04 +0000

    Fix for remediation :SessionScope-ServletSession-ServletContext Methods

--- src/java/validateservlet1.java
+++ src/java/validateservlet1.java
@@ -1,6 +1,3 @@
-
-//package validator;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -11,52 +8,37 @@ import java.sql.*;
 import javax.servlet.*;
 import javax.servlet.http.HttpSession;
 
-
 public class validateservlet1 extends HttpServlet {
 
     /**
-     *
      * @param request
      * @param response
      * @throws ServletException
      * @throws IOException
      */
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException{
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        
         String uname = request.getParameter("uname");
         String pass = request.getParameter("pass");
-      //  String usertype = request.getParameter("usertype");//its actually db name
-              
-             if(pass.equals("admin") && uname.equals("admin"))
-             {
-             
-            HttpSession session=request.getSession();
-            String username1="administrator";
-            session.setAttribute("username",username1);
-            session.setAttribute("id",uname);
-//            session.setAttribute("usertype",usertype);
-                RequestDispatcher rd = request.getRequestDispatcher("welcomeaccountantz.jsp");
-                rd.forward(request, response);
-            
-         }
-             else {
-              
-
-                //out.println("<h4 style=\"color:red;\">Invalid username & password</h4>");
-                response.sendRedirect("admin.jsp?id=Invalid username or password");
-                //RequestDispatcher rd = request.getRequestDispatcher("index.html");
-                //rd.include(request, response);
-                
-        
+        if (pass.equals("admin") && uname.equals("admin")) {
+            HttpSession session = request.getSession();
+            String username1 = "administrator";
+            session.setAttribute("username", username1);
+            session.setAttribute("id", uname);
+            // session.setAttribute("usertype",usertype);
+            RequestDispatcher rd = request.getRequestDispatcher("welcomeaccountantz.jsp");
+            rd.forward(request, response);
+        } else {
+            // out.println("<h4 style=\"color:red;\">Invalid username & password</h4>");
+            response.sendRedirect("admin.jsp?id=Invalid username or password");
+        // RequestDispatcher rd = request.getRequestDispatcher("index.html");
+        // rd.include(request, response);
         }
-       
-         
-       
+    }
+
+    public void printOutput() {
+        System.out.println("Added new Method using FEGO Remediations");
     }
 }
-
-   
